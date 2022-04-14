a0fa04728257f035fc312eef72177479953b34a3
Marco <mar.wenzel@gmx.de>
2022-04-14 20:34:35 +0200

    Initial Commit

---
+++ EaseeDemo/EaseeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EaseeDemoApp.swift
+//  EaseeDemo
+//
+//  Created by Marco Wenzel on 14.04.22.
+//
+
+import SwiftUI
+
+@main
+struct EaseeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
